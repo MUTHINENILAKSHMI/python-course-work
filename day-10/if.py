0e0efc06d0d0ffb925980bc67f6263d7a0ea2ca8
MUTHINENILAKSHMI <muthinenilakshmi8@gmail.com>
2026-08-17 19:42:04 +0530

    conditionalStatements

---
+++ day-10/if.py
@@ -0,0 +1,21 @@
+"""# best seller or not
+sales=int(input("enter the sales:"))
+if sales>1000:
+    print("Best Seller")"""
+
+"""# instagram verif status
+acc=eval(input("eligible account"))
+ver_sub=eval(input("meta Verified subscription: "))
+if acc and ver_sub:
+    print("Verified Badge Granted")"""
+
+#rain Status
+
+"""rain=eval(input("enter the rain status"))
+if rain:
+    print("Extra the charges aplied")"""
+
+#phone Charging
+phone_charge=eval(input("enter the charge"))
+if phone_charge<=20:
+    print("Low battery")
---
+++ day-10/ifelse.py
@@ -0,0 +1,21 @@
+"""#username and password
+username=input("enter a username")
+password=input("enter a  password")
+if username=="Lakshmi" and password=="Lakshmi123":
+    print("Login sucessfully")
+else:
+    print("Invaild Credentials")"""
+
+# products are found or not found
+"""list=input().split()
+search=input()
+if search in list:
+    print(f"{search} list found")
+else:
+    print(f"{search}  list not found")"""
+
+bil=int(input())
+if bil>99:
+    print(bil)
+else:
+    print(bil+30)
